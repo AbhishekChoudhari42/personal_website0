12ed89476f49848840458ec0147d5d8ba962ce37
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2021-09-06 15:07:05 +0530

    skills-section-update

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// preloader
+
 paceOptions = {
     ajax:true,
     document:true,
@@ -8,27 +11,86 @@ Pace.on('done',function(){
     document.querySelector('.pace').style.display = 'none';
 })
 
+
+
+// menu - animation
+
+
+
 var menu = document.querySelector('.menu');
-var menuSlide = document.querySelector('.menu-slide');
 var slide = document.querySelectorAll('.slide');
 var slideInfo= document.querySelector('.slide-info');
 
 
 
+
 menu.addEventListener('click',function(){
-    menuSlide.classList.toggle('open')
+    
+
+
+    // gsap.to(".slide", {translate: 100, duration: 1});
+
     menu.classList.toggle('cancel');
     slideInfo.classList.toggle('show')
 
     slide.forEach(element => {
-        element.classList.toggle('open')
+        element.classList.toggle('open1')
     });
    
+
+
 
     
     
     
 })
+let infoLink = document.querySelectorAll('.slide-info a');
+infoLink.forEach(element => {
+    element.addEventListener('click',function(){
+        menu.classList.toggle('cancel');
+        slideInfo.classList.toggle('show')
+    
+        slide.forEach(element => {
+            element.classList.toggle('open1')
+        });
+    })
+});
+
+// scroll
+
+var scrollToTop = document.querySelector('.scroll-to-top a');
+
+var lastscroll = 0;
+var navbar = document.querySelector('.navigation-bar');
+var op = 0
+window.addEventListener('scroll',function(){
+    console.log('show')
+    
+    
+
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollToTop.style.opacity = (scrollTop > 1000)?1:0
+    
+
+
+    if(scrollTop > lastscroll){
+        navbar.style.top = '-100%'
+        console.log('down')
+
+    }
+    else{
+        navbar.style.top = '0%'
+        console.log('up')
+        
+
+    }
+    lastscroll = scrollTop;
+})
+
+
+
+
+
 
 
 
@@ -59,23 +121,35 @@ function setup(){
     
     createCanvas(canvasWidth,canvasHeight,WEBGL);
     frameRate(30)
+   
 }
 
 let sphereWidth = canvasWidth>500?canvasWidth*0.15:canvasWidth*0.4
 let size;
+let c = 0
+setInterval(() => {
+    c = c==0?1:0
+}, 500);
 
 function draw(){
     size = (sphereWidth + window.pageYOffset)>1500?1500:sphereWidth + window.pageYOffset/5
-    background(10,10,10)
+    background(0,0,0)
 
-    stroke(250,0,0)
+    stroke(205,0,0)
     fill(0,0,0,0)
 
     let rX = (angle) + 0.005
     let rY = (angle) + 0.005
+  
     rotateX(rY/2)
     rotateY(rX/2)
     sphere(size) 
+
+    fill(255,0,0)
+
+   
+
+   
     angle+=0.01
 
 }
